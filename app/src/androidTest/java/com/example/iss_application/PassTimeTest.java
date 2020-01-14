37ffb2a7b2c81010fa77874e5895a8cea02eb8bc
ShamarcusW-MAC <56922234+ShamarcusW-MAC@users.noreply.github.com>
2020-01-14 18:29:01 -0500

    Added in Testing

--- app/src/androidTest/java/com/example/iss_application/PassTimeTest.java
+++ app/src/androidTest/java/com/example/iss_application/PassTimeTest.java
@@ -15,11 +15,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.swipeDown;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class PassTimeTest {
