6d702794a366610935db7023124a1efb5b4482bb
ShamarcusW-MAC <56922234+ShamarcusW-MAC@users.noreply.github.com>
2020-01-14 16:08:57 -0500

    Added in Testing

---
+++ app/src/androidTest/java/com/example/iss_application/PassTimeTest.java
@@ -0,0 +1,43 @@
+package com.example.iss_application;
+
+
+import android.os.SystemClock;
+
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.iss_application.view.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class PassTimeTest {
+
+    @Rule
+    public final ActivityScenarioRule<MainActivity> main = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testLaunch(){
+        onView(ViewMatchers.withId(R.id.passtime_recyclerview))
+                .check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void testRecyclerRefresh(){
+        SystemClock.sleep(5000);
+        onView(ViewMatchers.withId(R.id.swipe_recyclerview)).perform(swipeDown());
+    }
+
+}
--- app/src/test/java/com/example/iss_application/ExampleUnitTest.java
+++ app/src/test/java/com/example/iss_application/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package com.example.iss_application;
 
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +18,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void getDate() {
+        long milliSeconds = 1578933653;
+        DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
+        calendar.setTimeInMillis(milliSeconds * 1000);
+
+        assertEquals("01/13/2020", format.format(calendar.getTime()));
+
     }
 }
